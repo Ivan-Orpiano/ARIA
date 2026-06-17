9541b6147dc2830c80caad528e70ec7ebe6da6cc
Ivan Orpiano <ivanritchelorpiano@gmail.com>
2026-06-18 00:44:01 +0800

    Phase 1: Header Development

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import {
+  ChatIcon,
+  FilesIcon,
+  CalendarIcon,
+  MailIcon,
+  SettingsIcon,
+  SparkleIcon,
+  XIcon,
+} from '../icons/Icons';
+
+const NAV_ITEMS = [
+  { to: '/',         label: 'Chat',     icon: ChatIcon,     end: true },
+  { to: '/files',     label: 'Files',    icon: FilesIcon },
+  { to: '/calendar',  label: 'Calendar', icon: CalendarIcon },
+  { to: '/email',     label: 'Email',    icon: MailIcon },
+  { to: '/settings',  label: 'Settings', icon: SettingsIcon },
+];
