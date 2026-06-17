6f8389d79e5812d6eda8266c2f74aff7b3bdb445
Ivan Orpiano <ivanritchelorpiano@gmail.com>
2026-06-17 17:09:54 +0800

    Phase 1: Libraries Imported

--- src/App.jsx
+++ src/App.jsx
@@ -4,3 +4,11 @@ import './styles/globals.css';
 import { ChatProvider } from './context/ChatContext';
 import Sidebar from './components/layout/Sidebar';
 import Header  from './components/layout/Header';
+
+import ChatPage     from './pages/ChatPage';
+import FilesPage    from './pages/FilesPage';
+import CalendarPage from './pages/CalendarPage';
+import EmailPage    from './pages/EmailPage';
+import SettingsPage from './pages/SettingsPage';
+import NotFoundPage from './pages/NotFoundPage';
+
